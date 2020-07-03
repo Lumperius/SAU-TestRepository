4ceb8c2994fd63b5d8935361418e096b50e542d5
Lumperius <Fasttyr@gmail.com>
2020-07-03 16:35:04 +0300

    Create project
    Creating project, connecting to database and adding user model, registration controller and view.

---
+++ GoodMoodProvider/GoodMoodProvider/DataContexts/UserContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using GoodMoodProvider.Models;
+
+namespace GoodMoodProvider.DataContexts
+
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> ID { get; set; }
+        public DbSet<User> Nickname { get; set; }
+        public DbSet<User> FirstName { get; set; }
+        public DbSet<User> SecondName { get; set; }
+        public DbSet<User> Age { get; set; }
+        public DbSet<User> Gender { get; set; }
+        public DbSet<User> RegDate { get; set; }
+        public DbSet<User> IsOnline { get; set; }
+
+
+    }
+}
---
+++ GoodMoodProvider/GoodMoodProvider/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodMoodProvider.DataContexts
+{
+    public class User
+    {
+        public Guid ID { get; set; }
+        public string Nickname { get; set; }
+        public string Firstname { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public DateTime RegDate { get; set; }
+        public int IsOnline { get; set; }
+
+    }
+}
